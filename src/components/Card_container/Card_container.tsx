bb30bcfd455bb7f694c805e31ad34760c67e9742
LMCT04 <100221599+LMCT04@users.noreply.github.com>
2024-09-02 22:28:44 -0300

    offers cards and filter

--- src/components/Card_container/Card_container.tsx
+++ src/components/Card_container/Card_container.tsx
@@ -3,12 +3,42 @@ import styles from "./Card_container.module.css";
 
 const Card_container = () => {
   const productsArray = [
-    { id: 1, name: "Product 1", price: 100 },
-    { id: 2, name: "Product 2", price: 200 },
-    { id: 3, name: "Product 3", price: 300 },
-    { id: 4, name: "Product 4", price: 400 },
-    { id: 5, name: "Product 5", price: 500 },
-    { id: 6, name: "Product 6", price: 600 },
+    {
+      id: 1,
+      name: "Product 1",
+      price: 100,
+      off: { isActive: true, newPrice: 80 },
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 200,
+      off: { isActive: false, newPrice: null },
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 300,
+      off: { isActive: true, newPrice: 80 },
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 400,
+      off: { isActive: false, newPrice: null },
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      price: 500,
+      off: { isActive: true, newPrice: 80 },
+    },
+    {
+      id: 6,
+      name: "Product 6",
+      price: 600,
+      off: { isActive: false, newPrice: null },
+    },
   ];
 
   return (
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -3,6 +3,47 @@ import Slider from "../../components/Slider/Slider";
 import styles from "./Home.module.css";
 
 const Home = () => {
+  const productsArray = [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 100,
+      off: { isActive: true, newPrice: 80 },
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 200,
+      off: { isActive: false, newPrice: null },
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 300,
+      off: { isActive: true, newPrice: 80 },
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 400,
+      off: { isActive: false, newPrice: null },
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      price: 500,
+      off: { isActive: true, newPrice: 80 },
+    },
+    {
+      id: 6,
+      name: "Product 6",
+      price: 600,
+      off: { isActive: false, newPrice: null },
+    },
+  ];
+
+  const activeOffers = productsArray?.filter((product) => product.off.isActive);
+
   return (
     <Screen>
       <div className={styles["slider_container"]}>
@@ -14,7 +55,14 @@ const Home = () => {
         </div>
         <hr />
         <div className={styles["off_content"]}>
-          <p>offers ...</p>
+          {activeOffers.map((product) => {
+            return (
+              <div key={product.id} className={styles["off_card"]}>
+                <h3>{product.name}</h3>
+                <p>{product.off.newPrice}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </Screen>
